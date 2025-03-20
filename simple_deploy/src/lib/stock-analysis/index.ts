08238076b85a1d92c685a532c1b045fce74c551d
arun4319 <68957340+arun4319@users.noreply.github.com>
2025-03-20 15:26:56 -0400

    Delete simple_deploy directory

--- simple_deploy/src/lib/stock-analysis/index.ts
+++
@@ -1,9 +0,0 @@
-import { StockData } from '@/lib/stock-analysis/types';
-
-export function exportStockIndex() {
-  return {
-    from: './types',
-    from2: './data',
-    from3: './analyzer',
-  };
-}
